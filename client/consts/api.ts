22cd2d28ae8c4a76cb9c823da0e2f53baaf47c02
jaum3fp <jauferper@alu.edu.gva.es>
2025-05-12 19:50:22 +0200

    create api consts

---
+++ client/consts/api.ts
@@ -0,0 +1,13 @@
+export const API = {
+  restcountries: {
+    base: 'https://restcountries.com/v3.1',
+    alpha: 'https://restcountries.com/v3.1/alpha'
+  },
+  bitacoraForum: {
+    base: 'http://127.21.0.11:8080/api/v1',
+    posts: {
+      all: 'http://127.21.0.11:8080/api/v1/post/all',
+      countCountry: 'http://127.21.0.11:8080/api/v1/post/count/',
+    }
+  },
+}
