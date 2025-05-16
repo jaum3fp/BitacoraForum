41137de762adee319172fdf32a09ddfb61c103fc
jaum3fp <jauferper@alu.edu.gva.es>
2025-05-16 18:32:38 +0200

    add new profile image picker

---
+++ client/components/Forms/Fields/ProfileImageField.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+const modelValue = defineModel<string | null | undefined>()
+
+const defaultImage = 'https://github.com/benjamincanac.png'
+const fileInput = ref<HTMLInputElement | null>(null)
+
+const triggerFileInput = () => fileInput.value?.click()
+
+const onFileChange = (event: Event) => {
+  const input = event.target as HTMLInputElement
+  const file = input.files?.[0]
+  if (file) {
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      modelValue.value = e.target?.result as string
+    }
+    reader.readAsDataURL(file)
+  }
+}
+</script>
+
+<template>
+  <div class="relative w-[120px] group cursor-pointer">
+
+    <UAvatar :src="modelValue || defaultImage"  class="w-full h-full rounded-full">
+      <template #fallback>
+        <span class="w-full h-full flex items-center justify-center bg-gray-400 text-white text-sm font-medium rounded-full">
+          JD
+        </span>
+      </template>
+    </UAvatar>
+
+
+    <div class="absolute inset-0 bg-black/50 rounded-full flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity z-10"
+      @click="triggerFileInput"
+    >
+      <UIcon name="i-charm-camera" class="text-white text-2xl" />
+    </div>
+
+    <input ref="fileInput" type="file" accept="image/*" class="hidden" @change="onFileChange" />
+
+  </div>
+</template>
