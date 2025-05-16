c3e37710aa74ccc5be81053328d5fc2535552792
jaum3fp <jauferper@alu.edu.gva.es>
2025-05-16 18:43:52 +0200

    deactivate click on the water in the worldmap

--- client/components/Worldmap/Worldmap.vue
+++ client/components/Worldmap/Worldmap.vue
@@ -3,15 +3,22 @@
 import WorldmapSvg from '@/assets/svg/worldmap.svg'
 import CountryCard from './CountryCard.vue'
 
-const emit = defineEmits(['clickCountry'])
+const emit = defineEmits(['clickCountry', 'mouseoverCountry'])
 
-function handleWorldmapSVG(event: MouseEvent) {
+const handleWorldmapSvg = (event: MouseEvent) => {
   const target = event.target as SVGElement
   const cc = target.getAttribute('cc') || target.parentElement?.getAttribute('cc')
-
   console.log('Emit selectedCountry', cc)
-  emit('clickCountry', event, cc)
-  
+  return cc
+}
+
+const handleClick = (event: MouseEvent) => {console.log("click")
+  const cc = handleWorldmapSvg(event)
+  if (cc) emit('clickCountry', event, cc)
+}
+const handleMouseover = (event: MouseEvent) => {console.log("over")
+  const cc = handleWorldmapSvg(event)
+  emit('mouseoverCountry', event, cc)
 }
 
 </script>
@@ -27,8 +34,6 @@ function handleWorldmapSVG(event: MouseEvent) {
 
 <template>
 
-  <WorldmapSvg class="worldmap-svg" @mouseover="handleWorldmapSVG" />
-   
+<WorldmapSvg class="worldmap-svg" @click="handleClick" @mouseover="handleMouseover" />
+
 </template>
-
-
