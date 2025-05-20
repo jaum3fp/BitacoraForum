537eb0d91743b6aafccd1a1317b84f3f5b43dfa8
jaum3fp <jauferper@alu.edu.gva.es>
2025-05-20 10:30:05 +0200

    fix that the water on the world map emitted events.

--- client/pages/explore.vue
+++ client/pages/explore.vue
@@ -4,14 +4,28 @@ import CountryCard from '../components/Worldmap/CountryCard.vue';
 import { ref } from 'vue';
 
 
-const country = ref<string | null>(null)
+const cardCountry = ref<string | null>(null)
+const showCountryPosts = reactive<{ country: string, show: boolean }>({
+  country: '',
+  show: false,
+})
+
+defineShortcuts({
+  o: () => showCountryPosts.show = !showCountryPosts.show
+})
 
 const mapCoords = ref({ x: -1, y: -1 })
 
-function onSelectedCountry(event: MouseEvent, cc: string) {
+function onClickCountry(event: MouseEvent, cc: string) {
+  cardCountry.value = null
+  showCountryPosts.country = cc
+  showCountryPosts.show = true
+}
+
+function onMouseoverCountry(event: MouseEvent, cc: string) {
     const { offsetX: x, offsetY: y } = event
     mapCoords.value = { x, y }
-    country.value = cc
+    cardCountry.value = cc
 }
 
 </script>
@@ -19,18 +33,28 @@ function onSelectedCountry(event: MouseEvent, cc: string) {
 
 <template>
 
-    <div class="relative w-full h-screen">
-        <Worldmap class="absolute inset-o w-full h-full" @click-country="onSelectedCountry" />
-        <CountryCard 
-            v-if="country"
-            :country="country"
-            :style="{ top: `${mapCoords.y}px`, left: `${mapCoords.x}px` }"
-            class="absolute"
-        />
-    </div>
+<div class="explorepage relative w-full h-screen">
+
+    <Worldmap class="absolute inset-o w-full h-full"
+        @click-country="onClickCountry"
+        @mouseover-country="onMouseoverCountry"
+    />
+
+    <CountryCard v-if="cardCountry"
+        :country="cardCountry"
+        :style="{ top: `${mapCoords.y}px`, left: `${mapCoords.x}px` }"
+        class="absolute"
+    />
+
+    <USlideover v-model:open="showCountryPosts.show"
+          side="right"
+          :overlay="true"
+          :ui="{ content: 'w-[60%] max-w-none', }"
+        >
+        <template #body>
+            <PostsList :country="showCountryPosts.country" />
+        </template>
+    </USlideover>
+</div>
 
 </template>
-
-
-<style scoped>
-</style>
